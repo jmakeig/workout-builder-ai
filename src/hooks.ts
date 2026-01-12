cb9050ab39732854488d98b4e0d9e69d8f6a604f
Justin Makeig <jmpublic@makeig.com>
2026-01-12 00:12:08 +0000

    Starts from scratch(ish)

--- src/hooks.ts
+++
@@ -1,9 +0,0 @@
-import type { Transport } from '@sveltejs/kit';
-import { Validation } from '$lib/validation';
-
-export const transport: Transport = {
-	Validation: {
-		encode: (validation) => validation instanceof Validation && validation.issues(),
-		decode: (validation) => Validation.fromJSON(validation)
-	}
-};
--- src/routes/exercises/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { exercise_api } from '$lib/api';
-
-export function load() {
-	const exercises = exercise_api.list();
-	return {
-		exercises: exercises.map((e) => e.toJSON())
-	};
-}
